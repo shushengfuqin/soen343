bab697aca37a903dffa62f7e88119721b2f4fff3
Ruochen Jia <ruocjia@gmail.com>
2020-10-02 12:29:41 -0400

    backend interraction modification

--- src/soen343/Main.java
+++ src/soen343/Main.java
@@ -5,10 +5,15 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    Stage window;
+    Scene home, light;
 
     public static void main(String[] args) {
 	    //start the problem
@@ -17,45 +22,28 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Home Page");
+        window = primaryStage;
 
-        //Create different button needs
-        Button button1 = new Button();
-        button1.setText("Light");
-        Button button2 = new Button();
-        button2.setText("Heat");
+        Label label1 = new Label("Home");
+        Button button1 = new Button("Light");
+        button1.setOnAction(e -> window.setScene(light));
 
-        //This class will handle the light button
-        button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                //open of close the light
-                int i = 0;
-                if(i == 0){
-                    System.out.println("You opened the light");
-                    i = 1;
-                } else {
-                    System.out.println("You closed the light");
-                    i = 0;
-                }
-            }
-        });
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1,button1);
+        home = new Scene(layout1,400,400);
 
-        //This class will handle the heating button
-        button2.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("You opened the heating system page");
-            }
-        });
+        //Button2
+        Button button2 = new Button("Light");
+        button2.setOnAction(e -> window.setScene(home));
 
+        //Layout 2
+        VBox layout2 = new VBox(20);
+        Label label2 = new Label("Light");
+        layout2.getChildren().addAll(label2,button2);
+        light = new Scene(layout2,500,250);
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button1);
-        layout.getChildren().add(button2);
-
-        Scene scene = new Scene(layout,1000,750);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        window.setScene(home);
+        window.setTitle("SHS");
+        window.show();
     }
 }
