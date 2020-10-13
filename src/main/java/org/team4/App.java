7df33742c3524dee9260103a65293f5b166974d6
Ruochen Jia <ruocjia@gmail.com>
2020-10-12 22:41:57 -0400

    UI modification

--- src/main/java/org/team4/App.java
+++ src/main/java/org/team4/App.java
@@ -1,11 +1,14 @@
 package org.team4;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -16,10 +19,27 @@ public class App extends Application {
 
     private static Scene scene;
     private static Stage window;
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"));
+        stage.initStyle(StageStyle.UNDECORATED);
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
         stage.setScene(scene);
         stage.show();
 
