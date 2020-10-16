e10edd928e964d141c9c2c0cc4b204adc640cd38
phong1233 <phong.le0499@gmail.com>
2020-10-16 16:47:56 -0400

    fix conflicts

--- src/main/java/org/team4/App.java
+++ src/main/java/org/team4/App.java
@@ -3,12 +3,6 @@ package org.team4;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-<<<<<<< HEAD
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-=======
->>>>>>> 75efca8... Add implemenation of house layout in dashboard
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import java.io.IOException;
@@ -19,11 +13,6 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-<<<<<<< HEAD
-    private double xOffset = 0;
-    private double yOffset = 0;
-=======
->>>>>>> 75efca8... Add implemenation of house layout in dashboard
     public static FXMLLoader fxmlLoader;
 
     @Override
@@ -32,25 +21,8 @@ public class App extends Application {
         scene = new Scene(fxmlLoader.load());
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Dashboard");
-<<<<<<< HEAD
-        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                xOffset = event.getSceneX();
-                yOffset = event.getSceneY();
-            }
-        });
-        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                stage.setX(event.getScreenX() - xOffset);
-                stage.setY(event.getScreenY() - yOffset);
-            }
-        });
-=======
         stage.setScene(scene);
         stage.show();
->>>>>>> 75efca8... Add implemenation of house layout in dashboard
     }
 
     public static void main(String[] args) throws IOException {
