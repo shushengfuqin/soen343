2abf87ebced0cf53b25bf153a65fa74490bc8601
immaroot <killiankelly@gmail.com>
2020-10-29 20:12:59 -0400

    refactored some methods

--- src/main/java/org/team4/shsParameters/ShsParameterService.java
+++ src/main/java/org/team4/shsParameters/ShsParameterService.java
@@ -2,64 +2,32 @@ package org.team4.shsParameters;
 
 import org.team4.common.Settings;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
+import java.util.Calendar;
+import java.util.stream.Stream;
 
 public class ShsParameterService {
 
-    /**
-     * Set the new date of the simulation
-     * @param date
-     * @param hours
-     * @param minutes
-     * @param seconds
-     * @return
-     */
-    public boolean setDate(Date date, int hours, int minutes, int seconds) {
-        date.setHours(hours);
-        date.setMinutes(minutes);
-        date.setSeconds(seconds);
-        Settings.simulationTime.setDateTime(date);
-        return true;
+    public ShsParameterService() {
     }
 
-    /**
-     * Set the new time of the simulation
-     * @param localDate
-     * @param time
-     * @return
-     */
-    public boolean setDateTime(LocalDate localDate, String time) {
-        if(time == null || localDate == null) {
+    public boolean setDateTime(LocalDate localDate, String timeInput) {
+
+        if (localDate == null || timeInput == null)
             return false;
-        }
+        if (!timeInput.matches("^(?:(?:([01]?\\d|2[0-3]):)?([0-5]?\\d):)?([0-5]?\\d)$"))
+            return false;
 
-        String[] timeArr = time.split(":");
-        int[] timeIntArr = new int[3];
-        try {
-            if(timeArr.length == 3) {
-                timeIntArr[0] = Integer.parseInt(timeArr[0]);
+        int[] time = Stream.of(timeInput.split(":")).mapToInt(Integer::parseInt).toArray();
 
-                if (timeIntArr[0] > 24 || timeIntArr[0] < 0) throw new NumberFormatException();
-
-                timeIntArr[1] = Integer.parseInt(timeArr[1]);
-                if (timeIntArr[1] > 60 || timeIntArr[1] < 0) throw new NumberFormatException();
-
-                timeIntArr[2] = Integer.parseInt(timeArr[2]);
-                if (timeIntArr[2] > 60 || timeIntArr[2] < 0) throw new NumberFormatException();
-
-                Instant instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
-                Date date = Date.from(instant);
-                setDate(date, timeIntArr[0], timeIntArr[1], timeIntArr[2]);
-                return true;
-            }
-        }
-        catch (NumberFormatException e) {
-            e.printStackTrace();
-            System.out.println("Invalid time format");
-        }
-        return false;
-    }
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, localDate.getYear());
+        calendar.set(Calendar.MONTH, localDate.getMonthValue());
+        calendar.set(Calendar.DAY_OF_MONTH, localDate.getDayOfMonth());
+        calendar.set(Calendar.HOUR, time[0]);
+        calendar.set(Calendar.MINUTE, time[1]);
+        calendar.set(Calendar.SECOND, time[2]);
+        Settings.simulationTime.setDateTime(calendar);
+        return true;
+   }
 }
