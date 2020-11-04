414bac73017075a391aadb54bb2c1300e7f91de2
phong1233 <phong.le0499@gmail.com>
2020-11-03 19:39:06 -0500

    implementation of permission

--- src/main/java/org/team4/simulationParameters/Permission.java
+++ src/main/java/org/team4/simulationParameters/Permission.java
@@ -1,16 +1,117 @@
 package org.team4.simulationParameters;
 
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.team4.common.Settings;
 import org.team4.user.User;
 import org.team4.user.UserService;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Permission {
 
-    public Permission(){          }
+    //family, guest, stranger, adult, location
+    public static boolean windowPermission[]  = {true, true, false, true, true};
+    public static boolean doorPermission[]  = {true, true, false, true, true};
+    public static boolean lightPermission[]  = {true, true, false, true, true};
+
+    public static UserService userService = new UserService();;
+    public Permission() { }
+
+    //Todo: do this method for doors and light
+    public static boolean checkUserWindowPermission(int x, int y) {
+        String currUser = Settings.currentUser;
+        if(currUser == null) return true;
+
+        User user = userService.getSingleUser(currUser);
+
+        if(windowPermission[3] && !user.isAdult()) return false;
+
+        if(windowPermission[4] && (user.getX() != x || user.getX() != y)) return false;
+
+        switch (user.getStatus()) {
+            case "family":
+                return windowPermission[0];
+            case "guest":
+                return windowPermission[1];
+            case "stranger":
+                return windowPermission[2];
+        }
+        return false;
+    }
+
+    public static void saveNewPermission(boolean[] window) {
+        windowPermission = window;
+        //todo: Door permission and light permission
+        //todo: Save the new permission to the file
+        savePermissionToFile();
+    }
 
 
+    public static void savePermissionToFile() {
+        JSONObject jo = new JSONObject();
+        jo.put("windowPermissions", windowPermission);
+        //todo: add door permission and light permission
 
+        String permissionStr = jo.toString();
+        writeToPermissionFile(permissionStr);
+    }
 
+    public static void updatePermissionsFromFile() {
+        File userFile = new File("permissionFileName.json");
+        if(!userFile.exists()) return;
 
+        String data = readFromPermissionFile();
+
+        JSONObject jo = new JSONObject(data);
+        JSONArray arr = jo.getJSONArray("windowPermissions");
+        for(int i = 0; i < windowPermission.length; i++) {
+            windowPermission[i] = arr.getBoolean(i);
+            //todo: Do the same for doors and light
+        }
+    }
+
+    /**
+     * Read the data from a user file
+     * @return a string of an array results shows the permission of user
+     * @throws IOException
+     */
+    public static String readFromPermissionFile() {
+        File userFile = new File("permissionFileName.json");
+        if (userFile.exists()) {
+            Scanner fileReader = null;
+            try {
+                fileReader = new Scanner(userFile);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            String data = fileReader.nextLine();
+            return data;
+        }
+        return null;
+    }
+    /**
+     * Writes a string to the permission file
+     * @param
+     * @return
+     * @throws IOException
+     */
+    public static boolean writeToPermissionFile(String s) {
+        try {
+            FileWriter myWriter = new FileWriter("permissionFileName.json");
+            myWriter.write(s);
+            myWriter.close();
+            return true;
+        }
+        catch (IOException e) {
+            System.out.println("Error occured while writing to org.team4.permission file");
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
--- src/main/java/org/team4/simulationParameters/permissionCheckController.java
+++ src/main/java/org/team4/simulationParameters/permissionCheckController.java
@@ -2,22 +2,8 @@ package org.team4.simulationParameters;
 
 
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-import java.util.Scanner;
-
-import org.json.JSONObject;
-import org.team4.common.Settings;
-import org.team4.user.User;
-import org.team4.user.UserService;
-
-
+import javafx.stage.Stage;
 
 public class permissionCheckController
 {
@@ -67,227 +53,29 @@ public class permissionCheckController
     @FXML
     public CheckBox doorL;
 
+    public void save() {
+        boolean wa = windowA.isSelected();
+        boolean wc = windowC.isSelected();
+        boolean wg = windowG.isSelected();
+        boolean ws = windowS.isSelected();
+        boolean wl = windowL.isSelected();
+        boolean windowPermission[]  = {wa,wc,wg,ws,wl};
+        //todo: add the array for doors and light
+        //method should look like this:
+        // Permission.saveNewPermission(windowPermission, doorPermission, lightPermission);
+        Permission.saveNewPermission(windowPermission);
 
-    public boolean la = lightA.isSelected();
-    public boolean lc = lightC.isSelected();
-    public boolean lg = lightG.isSelected();
-    public boolean ls = lightS.isSelected();
-    public boolean wa = windowA.isSelected();
-    public boolean wc = windowC.isSelected();
-    public boolean wg = windowG.isSelected();
-    public boolean ws = windowS.isSelected();
-    public boolean da = doorA.isSelected();
-    public boolean dc = doorC.isSelected();
-    public boolean dg = doorG.isSelected();
-    public boolean ds = doorS.isSelected();
-    public boolean ll = lightL.isSelected();
-    public boolean wl = windowL.isSelected();
-    public boolean dl = doorL.isSelected();
-
-    //permission for Adults,Children,Guests,Stranger,Location
-    public boolean windowPermission []  = {wa,wc,wg,ws,wl} ;
-    public boolean lightPermission[] = {la,lc,lg,ls,ll};
-    public boolean doorPermission [] = {da,dc,dg,ds,dl};
-
-    UserService userService = new UserService();
-
-    // check the permission of control window
-    public boolean checkWindowPermission(int x, int y){
-        String currUser = Settings.currentUser;
-        if(currUser == null) return true;
-
-        User user = userService.getSingleUser(currUser);
-        int xUser = user.getX();
-        int yUser = user.getY();
-
-        if(currUser.equals("Adult")){
-            if(windowPermission[0]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        if(currUser.equals("Children")){
-            if(windowPermission[1]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        if(currUser.equals("Guest")){
-            if(windowPermission[2]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        if(currUser.equals("Stranger")){
-            if(windowPermission[3]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        if(currUser.equals("Location")){
-            if(windowPermission[4]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        return false;
+        //this will close the window
+        Stage stage = (Stage) windowC.getScene().getWindow();
+        stage.close();
     }
 
+    public void initialize(){
+        Permission.updatePermissionsFromFile();
+        boolean[] windowPermission = Permission.windowPermission;
+        windowA.setSelected(windowPermission[0]);
+        //todo: Set the default values for all checkbox
 
-
-
-    //Check Permission for control light
-    public boolean checkLightPermission(int x, int y){
-        String currUser = Settings.currentUser;
-        if(currUser == null) return true;
-
-        User user = userService.getSingleUser(currUser);
-        int xUser = user.getX();
-        int yUser = user.getY();
-
-        if(currUser.equals("Adult")){
-            if(lightPermission[0]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        if(currUser.equals("Children")){
-            if(lightPermission[1]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        if(currUser.equals("Guest")){
-            if(lightPermission[2]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        if(currUser.equals("Stranger")){
-            if(lightPermission[3]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        if(currUser.equals("Location")){
-            if(lightPermission[4]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-
-        return false;
     }
-
-
-    //Check Permission for Door
-    public boolean checkDoorPermission(int x, int y){
-        String currUser = Settings.currentUser;
-        if(currUser == null) return true;
-
-        User user = userService.getSingleUser(currUser);
-        int xUser = user.getX();
-        int yUser = user.getY();
-
-        if(currUser.equals("Adult")){
-            if(doorPermission[0]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        if(currUser.equals("Children")){
-            if(doorPermission[1]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        if(currUser.equals("Guest")){
-            if(doorPermission[2]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        if(currUser.equals("Stranger")){
-            if(doorPermission[3]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-        if(currUser.equals("Location")){
-            if(doorPermission[4]&& xUser==x && yUser==y){
-                return true;
-            }
-        }
-
-
-        return false;
-    }
-
-
-
-    int x;
-    int y;
-
-   public void initialize(){
-    this.checkWindowPermission(x,y);
-    this.checkLightPermission(x,y);
-    this.checkDoorPermission(x,y);
-}
-
-    private JSONObject toJson() {
-        JSONObject jo = new JSONObject();
-        jo.put("windowPermission", this.windowPermission);
-        jo.put("lightPermission", this.lightPermission);
-        jo.put("doorPermission", this.doorPermission);
-
-        return jo;
-    }
-
-    public String toString() {
-        return "windowPermission " + this.windowPermission + "\nlightPermission: " + this.lightPermission + "\ndoorPermission: " + this.doorPermission ;
-    }
-
-
-
-
-
-    /**
-     * Read the data from a user file
-     * @return a string of an array results shows the permission of user
-     * @throws IOException
-     */
-    public static String readFromPermissionFile() throws IOException {
-        File userFile = new File("permissionFileName");
-        if (userFile.exists()) {
-            Scanner fileReader = new Scanner(userFile);
-            String data = fileReader.nextLine();
-            return data;
-        }
-        return null;
-    }
-    /**
-     * Writes a string to the permission file
-     * @param
-     * @return
-     * @throws IOException
-     */
-    public static boolean writeToPermissionFile(String s) throws IOException {
-        try {
-            FileWriter myWriter = new FileWriter("permissionFileName.json");
-            myWriter.write(s);
-            myWriter.close();
-            return true;
-        }
-        catch (IOException e) {
-            System.out.println("Error occured while writing to org.team4.permission file");
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-
-
-
-
 
 }
